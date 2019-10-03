2e170530b72b574895246862c4886453aa772088
Raman <vr4207@intl.att.com>
2019-10-03 19:18:48 +0530

    first

---
+++ kudvenkat/src/app/employees/list-employees.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../models/employee.model';
+
+@Component({
+  selector: 'app-list-employees',
+  templateUrl: './list-employees.component.html',
+  styleUrls: ['./list-employees.component.css']
+})
+export class ListEmployeesComponent implements OnInit {
+
+  employees: Employee[] = [
+    {
+      id: 1,
+    name: "Mark",
+    gender: "Male",
+    email: "mark@gmail.com",
+    contactPreference: "email",
+    dateofBirth: new Date(),
+    department: "IT",
+    isActive: true,
+    photoPath: 'assets/images/mark.png'
+    },
+    {
+      id: 2,
+    name: "Mary",
+    gender: "Female",
+    phoneNumber: "9900099000",
+    contactPreference: "Phone",
+    dateofBirth: new Date(),
+    department: "HR",
+    isActive: true,
+    photoPath: 'assets/images/mary.png'
+    },
+    {
+      id: 3,
+    name: "John",
+    gender: "Male",
+    phoneNumber: "9900399002",
+    contactPreference: "Phone",
+    dateofBirth: new Date(),
+    department: "IT",
+    isActive: false,
+    photoPath: 'assets/images/john.png'
+    }
+  ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ kudvenkat/src/app/models/employee.model.ts
@@ -0,0 +1,13 @@
+export class Employee {
+    id: number;
+    name: string;
+    gender: string;
+    email?: string;
+    phoneNumber?: string;
+    contactPreference: string;
+    dateofBirth: Date;
+    department: string;
+    isActive: boolean;
+    photoPath?: string;
+
+}
