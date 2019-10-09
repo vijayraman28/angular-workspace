0ee622073d3d67ed343601b9274e26ff53361163
Raman <vr4207@intl.att.com>
2019-10-09 20:20:20 +0530

    second

--- kudvenkat/src/app/app.module.ts
+++ kudvenkat/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListEmployeesComponent } from './employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee.component';
+
+const appRoutes: Routes = [
+  {path:'list', component: ListEmployeesComponent},
+  {path:'create', component: CreateEmployeeComponent},
+  {path:'', redirectTo: '/list', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListEmployeesComponent
+    ListEmployeesComponent,
+    CreateEmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    BsDatepickerModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ kudvenkat/src/app/employees/create-employee.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Department } from '../models/department.model';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+departments: Department[] = [
+  {id: 1, name: 'Help Desk'},
+  {id: 2, name: 'HR'},
+  {id: 3, name: 'IT'},
+  {id: 4, name: 'Payroll'}
+];
+ 
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  saveEmployee(empForm: NgForm): void{
+    console.log("inside",empForm.value)
+  }
+}
