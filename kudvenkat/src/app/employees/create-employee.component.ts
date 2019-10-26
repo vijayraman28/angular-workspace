10393ad7ebd139c44f0ca73d400b26c539e314f9
Raman, Vijaya (vr4207) <vr4207@intl.att.com>
2019-10-26 13:38:48 +0530

    update

--- kudvenkat/src/app/employees/create-employee.component.ts
+++ kudvenkat/src/app/employees/create-employee.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Department } from '../models/department.model';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+import { Employee } from '../models/employee.model';
+
 
 @Component({
   selector: 'app-create-employee',
@@ -10,6 +12,20 @@ import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 })
 export class CreateEmployeeComponent implements OnInit {
   datePickerConfig:Partial<BsDatepickerConfig>;
+  previewPhoto = false;
+  employee: Employee = {
+    id: null,
+    name: null,
+    gender: null,
+    email: null,
+    phoneNumber: null,
+    contactPreference: null,
+    dateofBirth: null,
+    department: null,
+    isActive: null,
+    photoPath: null
+  }
+
 departments: Department[] = [
   {id: 1, name: 'Help Desk'},
   {id: 2, name: 'HR'},
@@ -18,13 +34,20 @@ departments: Department[] = [
 ];
  
   constructor() {
-    this.datePickerConfig = Object.assign({}, {containerClass: 'theme-dark-blue'});
+    this.datePickerConfig = Object.assign({}, {
+      containerClass: 'theme-dark-blue',
+      dateInputFormat: 'DD/MM/YYYY'
+    });
+   }
+
+   togglePhotoPreview(){
+     this.previewPhoto = !this.previewPhoto;
    }
 
   ngOnInit() {
   }
 
-  saveEmployee(empForm: NgForm): void{
-    console.log("inside",empForm.value)
+  saveEmployee(Newemployee: Employee): void{
+    console.log("inside",Newemployee)
   }
 }
